6d45bec65dc83ec32ffb3077c881e3290933948c
Kalmykov Stanislav <stanislav.kalmykov.job@gmail.com>
2022-05-17 22:34:27 +0300

    Was finished task1

---
+++ task1.php
@@ -0,0 +1,31 @@
+<?php
+
+	class test {
+		public $next;
+	}
+
+	$a = new test();
+	$b = new test();
+	$c = new test();
+	$a->next = $b;
+	$b->next = $c;
+	$c->next = null;
+
+	function reverse($a) 
+    {
+        // для удобства и наглядности можно передать в while срзу "a"
+		$curItem = $a;
+		$prevItem = null;
+		while ($curItem != null) {
+			$nextItem = $curItem->next;
+			$curItem->next = $prevItem;
+			$prevItem = $curItem;
+			$curItem = $nextItem;
+		}
+		return $prevItem;
+	}
+
+	$ob1 = reverse($a);
+	var_dump($ob1);
+
+?>
